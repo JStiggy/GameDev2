5ace78247b55bbd14fcf6e0f4b691f26c820d05a
John Steigerwald <johnstiggy@gmail.com>
2017-02-22 14:36:58 -0500

    Magnetization for Objects

--- Assets/Scripts/Magnetizable.cs
+++ Assets/Scripts/Magnetizable.cs
@@ -2,19 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Magnetizable : MonoBehaviour {
 
     public float magneticRadius = 10f;
-    bool magnetized = false;
+    public bool magnetized = true;
+    public float force = 200f;
 
-    //This script assumes that a Collider is attached
-    //A Rigidbody is not needed but can be attached
-	
-	// Update is called once per frame
-	void Update () {
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+	void FixedUpdate () {
 		if(magnetized)
         {
-
+            float distance = float.MaxValue;
+            Vector3 movementPosition = this.transform.position;
+            foreach (Collider2D col in Physics2D.OverlapCircleAll(this.transform.position, magneticRadius, 1<<9))
+            {
+                print("Update");
+                if (distance > Vector3.Distance(transform.position, col.transform.position))
+                {
+                    distance = Vector3.Distance(transform.position, col.transform.position);
+                    movementPosition = col.transform.position;
+                }
+            }
+            if(movementPosition != this.transform.position)
+            {
+                print("Force added");
+                rb.AddForce((movementPosition - transform.position).normalized * Time.deltaTime * force,ForceMode2D.Impulse);
+            }
         }
 	}
 }
