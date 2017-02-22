1e3a8bb04a500686755238d6fec111d0581e28f5
John Steigerwald <johnstiggy@gmail.com>
2017-02-22 12:20:06 -0500

    Fixed Folder

---
+++ Assets/Scripts/Magnetizable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnetizable : MonoBehaviour {
+
+    public float magneticRadius = 10f;
+    bool magnetized = false;
+
+    //This script assumes that a Collider is attached
+    //A Rigidbody is not needed but can be attached
+	
+	// Update is called once per frame
+	void Update () {
+		if(magnetized)
+        {
+
+        }
+	}
+}
