7335664e34d2329657926c90aa939878d805eb62
John Steigerwald <johnstiggy@gmail.com>
2017-03-03 14:16:17 -0500

    Animation for recharge station

---
+++ Assets/Scripts/RechargeStation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RechargeStation : MonoBehaviour {
+
+    Animator anim;
+      
+    //void OnEnable()
+    //{
+    //    if (anim == null)
+    //    {
+    //        anim = this.GetComponent<Animator>();
+    //    }
+    //    Play animation GameObject.FindGameObjectsWithTag("Player")[0];
+
+    //}
+
+
+
+}
