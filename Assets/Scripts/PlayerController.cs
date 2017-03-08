aeeed49c1b7f79c82399039988d6f8ae83af487b
John Steigerwald <johnstiggy@gmail.com>
2017-03-07 21:27:41 -0500

    Animator as far as im getting it
    All the anim play but some jank

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-//This script should be on the character sprite
-
-/* Keys config:
- * horizontal axis: 	horizontal motion
- * vertical axis: 		jumping
- * I:					accelerated mobility
- * U:					enhanced boost
- */
 public class PlayerController : MonoBehaviour
 {
     public float WalkSpeed;
@@ -20,6 +11,9 @@ public class PlayerController : MonoBehaviour
 
     public float moveDirection;
 
+    private Animator anim;
+    private Rigidbody2D rb;
+
     private bool Grounded = false;
     private bool BasicJump = true;  //Avoid input loss
     private bool EnhancedBoost = false;
@@ -28,34 +22,48 @@ public class PlayerController : MonoBehaviour
     private bool Glide = false;
     private bool EndGlide = false;
 
+    void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
+    }
+
     void Update()
     {
         //Walking
         moveDirection = Input.GetAxisRaw("Horizontal");
-        Grounded = Physics2D.OverlapCircle(transform.position - new Vector3(0, .5f, 0), .4f);
-        Glide = Input.GetKey(KeyCode.U);
+        Grounded = Physics2D.OverlapCircle(transform.position - new Vector3(0, .8f, 0), .4f, (1<<11));
+    
         EndGlide = Input.GetKeyUp(KeyCode.U);
+        Glide = Input.GetKey(KeyCode.U);
+
+        anim.SetBool("grounded", Grounded);
+        anim.SetBool("gliding", Glide);
+        anim.SetFloat("xVel", moveDirection);
     }
 
     void FixedUpdate()
     {
-        GetComponent<Rigidbody2D>().AddForce(transform.right * WalkSpeed * moveDirection);
+        rb.velocity = new Vector3(1 * WalkSpeed * moveDirection, rb.velocity.y, 0);
         print(transform.right * WalkSpeed * moveDirection);
         if (Glide)
         {
             EndGlide = false;
-            float vy = GetComponent<Rigidbody2D>().velocity.y;
+            float vy = rb.velocity.y;
             if (vy < 0f)
             {
-                GetComponent<Rigidbody2D>().AddForce(transform.up *
-                GetComponent<Rigidbody2D>().gravityScale * 0.9f * (-Physics2D.gravity.y));
+                rb.AddForce(transform.up * rb.gravityScale * 0.9f * (-Physics2D.gravity.y));
             }
         }
         if(EndGlide)
         {
             Glide = false;
-            GetComponent<Rigidbody2D>().AddForce(transform.up *
-                GetComponent<Rigidbody2D>().gravityScale * 0.9f * Physics2D.gravity.y);
+            rb.AddForce(transform.up * rb.gravityScale * 0.9f * Physics2D.gravity.y);
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position - new Vector3(0, .8f, 0), .4f);
     }
 }
