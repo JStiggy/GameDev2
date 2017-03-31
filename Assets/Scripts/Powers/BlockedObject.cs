328a2f5885e3299720295915fc65fcad64b128cf
SimonJ-PC\SimonJ9 <simonj142857@gmail.com>
2017-03-31 14:35:25 -0400

    no message

---
+++ Assets/Scripts/Powers/BlockedObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Put this script on any object that can be blocked by the shield
+
+public class BlockedObject {
+    public bool DestroyUponBlock;
+    public bool BreakShield;
+}
---
+++ Assets/Scripts/Powers/Shield.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+    
+    public float force_returned_co = 0.95f;
+    
+    private BlockedObject obj;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        obj = coll.gameObject.GetComponent<BlockedObject>();
+        if(obj != null)
+        {
+            coll.gameObject.GetComponent<Rigidbody2D>().velocity *= -force_returned_co;
+            if(obj.DestroyUponBlock)
+            {
+                Destroy(coll.gameObject);
+                return;
+            }
+            if(obj.BreakShield)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
