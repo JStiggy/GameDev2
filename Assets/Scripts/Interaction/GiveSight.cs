67dac605823c56a6970b1b3d6051dea70784733a
John Steigerwald <johnstiggy@gmail.com>
2017-04-12 15:30:11 -0400

    Chnages to lighting

--- Assets/Scripts/Interaction/GiveSight.cs
+++ Assets/Scripts/Interaction/GiveSight.cs
@@ -8,7 +8,7 @@ public class GiveSight : Interactable
     public override IEnumerator Interact()
     {
         print("D");
-        GameObject.FindGameObjectWithTag("Player").GetComponent<CreateShadows>().distance = 6;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<CreateShadows>().distance = 5;
         Destroy(this.gameObject);
         return null;
     }
