a543ac42f0f996b6afe4e290a9478a248b6a72d8
John Steigerwald <johnstiggy@gmail.com>
2017-04-28 16:36:07 -0400

    Final GameFest

---
+++ Assets/Scripts/Interaction/GiveBoost.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveBoost : Interactable
+{
+    void Start()
+    {
+        if ((((long)1 << 36) & GameManager.Manager.playerData.saveFlags) > 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public override IEnumerator Interact()
+    {
+        DialougeSystem sys = GameObject.Find("Dialouge System").GetComponent<DialougeSystem>();
+        sys.StartCoroutine("PrintDialouge", 10);
+
+        GameManager.FindObjectOfType<PlayerController>().AbilityCooldown.Glide = true;
+
+        GameManager.Manager.playerData.saveFlags = GameManager.Manager.playerData.saveFlags | ((long)1 << 36);
+        GameManager.Manager.playerData.Save(); //This would save it but need to test everything
+
+        Destroy(this.gameObject);
+        return null;
+    }
+}
